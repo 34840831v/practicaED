8c3ece8845dd499301f986589c0c53b61a38b4e0
Javier <Javier@LAPTOP-8Q3H0GFR>
2017-06-09 18:56:44 +0200

    Creamos la variable de clase_nombre

--- src/practicaED/Saluda.java
+++ src/practicaED/Saluda.java
@@ -1,8 +1,13 @@
 package practicaED;
 
+import java.util.Scanner;
+
 public class Saluda {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("introduzca nombre");
+		String nombre = teclado.nextLine();
 		System.out.println("Saludo impersonal");
 
 	}

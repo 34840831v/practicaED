ca61a83fced10242d34fac8bc64a71f2541ca3b4
Javier <Javier@LAPTOP-8Q3H0GFR>
2017-06-09 19:04:40 +0200

    Creamos metodo pideNombre()

--- src/practicaED/Saluda.java
+++ src/practicaED/Saluda.java
@@ -3,12 +3,23 @@ package practicaED;
 import java.util.Scanner;
 
 public class Saluda {
-
+	
+	public String nombre="";
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
 	public static void main(String[] args) {
+		
 		Scanner teclado = new Scanner(System.in);
 		System.out.println("introduzca nombre");
 		String nombre = teclado.nextLine();
-		System.out.println("Saludo impersonal");
+		System.out.println("hola " + nombre);
 
 	}
 
